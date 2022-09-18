fe1faf86eb57ce9ad4a3c4e06e4ebc0d48f6b39c
Piotr Jura <jura.piotrek@gmail.com>
2022-09-18 16:24:12 +0200

    Your First SPA / 005-persistent-layout

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,15 +1,19 @@
 <template>
-  <MainLayout>
-    <div>Index</div>
-    <Link href="/hello">Show Page</Link>
-    <div>
-      The message is {{ message }}
-    </div>
-  </MainLayout>
+  <div>Index</div>
+  <Link href="/hello">Show Page</Link>
+  <div>
+    The message is {{ message }}
+  </div>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3'
-import MainLayout from '../../Layouts/MainLayout.vue'
 defineProps({ message: String })
+</script>
+
+<script>
+import MainLayout from '../../Layouts/MainLayout.vue';
+export default {
+  layout: MainLayout
+}
 </script>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,11 +1,15 @@
 <template>
-  <MainLayout>
-    <div>Show</div>
-    <Link href="/">Main Page</Link>
-  </MainLayout>
+  <div>Show</div>
+  <Link href="/">Main Page</Link>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3'
+</script>
+
+<script>
 import MainLayout from '../../Layouts/MainLayout.vue';
+export default {
+  layout: MainLayout
+}
 </script>
