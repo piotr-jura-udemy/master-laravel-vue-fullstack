b52ce2110c9cca9e202f916f6970fe451d8290ae
Piotr Jura <jura.piotrek@gmail.com>
2022-09-24 17:14:18 +0200

    004-004-working-with-migrations

---
+++ database/migrations/2022_09_24_150311_add_fields_to_listings_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('listings', function (Blueprint $table) {
+            $table->unsignedTinyInteger('beds');
+            $table->unsignedTinyInteger('baths');
+            $table->unsignedTinyInteger('area');
+
+            $table->tinyText('city');
+            $table->tinyText('code');
+            $table->tinyText('street');
+            $table->tinyText('street_nr');
+
+            $table->unsignedInteger('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropColumns('listings', [
+            'beds', 'baths', 'area', 'city', 'code', 'street', 'street_nr', 'price'
+        ]);
+    }
+};
