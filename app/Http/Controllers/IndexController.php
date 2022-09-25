fd4dade19be3475ecf63703c9ba63ad42350fa70
Piotr Jura <jura.piotrek@gmail.com>
2022-09-25 14:55:24 +0200

    004-006-querying-database

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
+        // Uncomment the code below
+        // And write all the examples inside dd() call
+        // then visit http://127.0.0.1:8000 (or whatever php artisan serve suggests)
+        // to see the output
+        // dd(Listing::all());
         return inertia(
             'Index/Index',
             [
