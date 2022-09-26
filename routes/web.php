5587e00f15a86615d4843afe2ec3aae178d6c9c0
Piotr Jura <jura.piotrek@gmail.com>
2022-09-26 20:24:22 +0200

    004-009-resource-controller-listing-route-model-binding

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
+
+Route::resource('listing', ListingController::class)
+  ->only(['index', 'show']);
