361897ce00df128531eb6ea6da475d2a7b6ad15d
Piotr Jura <jura.piotrek@gmail.com>
2022-09-29 19:14:02 +0200

    004-014-computed-propeperties

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,13 +2,33 @@
   <Link href="/">Main Page</Link>&nbsp;
   <Link href="/hello">Show Page</Link>
   <!-- <div>The page with time {{ timer }}</div> -->
+  <div v-if="flashSuccess" class="success">
+    {{ flashSuccess }}
+  </div>
+  <div>{{ y }}</div>
   <slot>Default</slot>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/inertia-vue3'
+import { computed } from 'vue'
+import { Link, usePage } from '@inertiajs/inertia-vue3'
+
+// const x = ref(0)
+// const y = computed(() => x.value * 2)
+
+const page = usePage()
+const flashSuccess = computed(
+  () => page.props.value.flash.success,
+)
 // import { ref } from 'vue'
 
 // const timer = ref(0)
 // setInterval(() => timer.value++, 1000)
 </script>
+
+<style scoped>
+  .success {
+    background-color: green;
+    color: white;
+  }
+</style>
