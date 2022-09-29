bea4006b684020cff5e2948a564728022d744e9a
Piotr Jura <jura.piotrek@gmail.com>
2022-09-29 11:47:00 +0200

    004-013-sharing-data-middleware

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,14 +1,14 @@
 <template>
   <Link href="/">Main Page</Link>&nbsp;
   <Link href="/hello">Show Page</Link>
-  <div>The page with time {{ timer }}</div>
+  <!-- <div>The page with time {{ timer }}</div> -->
   <slot>Default</slot>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3'
-import { ref } from 'vue'
+// import { ref } from 'vue'
 
-const timer = ref(0)
-setInterval(() => timer.value++, 1000)
+// const timer = ref(0)
+// setInterval(() => timer.value++, 1000)
 </script>
