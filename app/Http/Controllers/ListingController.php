8c87ffda10853a722a58e50400fb07e00995cd9f
Piotr Jura <jura.piotrek@gmail.com>
2022-09-29 19:38:11 +0200

    004-015-data-validation

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -40,7 +40,18 @@ class ListingController extends Controller
      */
     public function store(Request $request)
     {
-        Listing::create($request->all());
+        Listing::create(
+            $request->validate([
+                'beds' => 'required|integer|min:0|max:20',
+                'baths' => 'required|integer|min:0|max:20',
+                'area' => 'required|integer|min:15|max:1500',
+                'city' => 'required',
+                'code' => 'required',
+                'street' => 'required',
+                'street_nr' => 'required|min:1|max:1000',
+                'price' => 'required|integer|min:1|max:20000000',
+            ])
+        );
 
         return redirect()->route('listing.index')
             ->with('success', 'Listing was created!');
--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -4,41 +4,65 @@
       <div>
         <label>Beds</label>
         <input v-model.number="form.beds" type="text" />
+        <div v-if="form.errors.beds">
+          {{ form.errors.beds }}
+        </div>
       </div>
 
       <div>
         <label>Baths</label>
         <input v-model.number="form.baths" type="text" />
+        <div v-if="form.errors.baths">
+          {{ form.errors.baths }}
+        </div>
       </div>
 
       <div>
         <label>Area</label>
         <input v-model.number="form.area" type="text" />
+        <div v-if="form.errors.area">
+          {{ form.errors.area }}
+        </div>
       </div>
 
       <div>
         <label>City</label>
         <input v-model="form.city" type="text" />
+        <div v-if="form.errors.city">
+          {{ form.errors.city }}
+        </div>
       </div>
 
       <div>
         <label>Post Code</label>
         <input v-model="form.code" type="text" />
+        <div v-if="form.errors.code">
+          {{ form.errors.code }}
+        </div>
       </div>
 
       <div>
         <label>Street</label>
         <input v-model="form.street" type="text" />
+        <div v-if="form.errors.street">
+          {{ form.errors.street }}
+        </div>
       </div>
 
       <div>
         <label>Street Nr</label>
         <input v-model.number="form.street_nr" type="text" />
+        <div v-if="form.errors.street_nr">
+          {{ form.errors.street_nr }}
+        </div>
       </div>
 
       <div>
         <label>Price</label>
         <input v-model.number="form.price" type="text" />
+        <div v-if="form.errors.price">
+          {{ form.errors.price }}
+        </div>
       </div>
 
       <div>
