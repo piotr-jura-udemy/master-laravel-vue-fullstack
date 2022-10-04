149dbe16c93151953776c047cbe406a7db9b40e4
Piotr Jura <jura.piotrek@gmail.com>
2022-10-04 19:17:36 +0200

    005-007-price-component

---
+++ resources/js/Components/Price.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>{{ formattedPrice }}</span>
+</template>
+
+<script setup>
+import {computed} from 'vue'
+const props = defineProps({
+  price: [Number, String],
+})
+const formattedPrice = computed(
+  () => Number(props.price).toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 0,
+  }),
+)
+</script>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -5,6 +5,7 @@
         <Link
           :href="route('listing.show', {listing: listing.id})"
         >
+          <Price :price="listing.price" class="text-2xl font-bold" />
           <ListingSpace :listing="listing" class="text-lg" />
           <ListingAddress :listing="listing" class="text-gray-500" />
         </Link>
@@ -33,6 +34,7 @@ import {Link} from '@inertiajs/inertia-vue3'
 import ListingAddress from '@/Components/ListingAddress.vue'
 import Box from '@/Components/UI/Box.vue'
 import ListingSpace from '@/Components/ListingSpace.vue'
+import Price from '@/Components/Price.vue'
 defineProps({
   listings: Array,
 })
