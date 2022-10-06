e9a024547ad53df7655a0782baa25605d8436c39
Piotr Jura <jura.piotrek@gmail.com>
2022-10-06 17:34:12 +0200

    005-012-progress-indicator

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,15 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import MainLayout from '@/Layouts/MainLayout.vue'
 import { ZiggyVue } from 'ziggy'
+import { InertiaProgress } from '@inertiajs/progress'
 import '../css/app.css'
+
+InertiaProgress.init({
+  delay: 0,
+  color: '#29d',
+  includeCSS: true,
+  showSpinner: true,
+})
 
 createInertiaApp({
   resolve: async (name) => {
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Larazillow</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
+
     @routes
     @vite('resources/js/app.js')
     @inertiaHead
