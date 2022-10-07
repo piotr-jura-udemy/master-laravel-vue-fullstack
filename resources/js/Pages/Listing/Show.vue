c016147bd43c27c86d01b48db57852cd1edaa0bf
Piotr Jura <jura.piotrek@gmail.com>
2022-10-07 20:11:08 +0200

    006-002-monthly-payment-logic

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -18,21 +18,23 @@
           Monthly Payment
         </template>
         <div>
-          <label class="label">Interest rate (2.5%)</label>
+          <label class="label">Interest rate ({{ interestRate }}%)</label>
           <input
+            v-model.number="interestRate"
             type="range" min="0.1" max="30" step="0.1"
             class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
           />
 
-          <label class="label">Duration (25 years)</label>
+          <label class="label">Duration ({{ duration }} years)</label>
           <input
+            v-model.number="duration"
             type="range" min="3" max="35" step="1"
             class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
           />
 
           <div class="text-gray-600 dark:text-gray-300 mt-2">
             <div class="text-gray-400">Your monthly payment</div>
-            <Price :price="500" class="text-3xl" />
+            <Price :price="monthlyPayment" class="text-3xl" />
           </div>
         </div>
       </Box>
@@ -45,7 +47,21 @@ import ListingAddress from '@/Components/ListingAddress.vue'
 import ListingSpace from '@/Components/ListingSpace.vue'
 import Price from '@/Components/Price.vue'
 import Box from '@/Components/UI/Box.vue'
-defineProps({
+
+import {ref, computed} from 'vue'
+
+const interestRate = ref(2.5)
+const duration = ref(25)
+
+const props = defineProps({
   listing: Object,
+})
+
+const monthlyPayment = computed(() => {
+  const principle = props.listing.price
+  const monthlyInterest = interestRate.value / 100 / 12
+  const numberOfPaymentMonths = duration.value * 12
+
+  return principle * monthlyInterest * (Math.pow(1 + monthlyInterest, numberOfPaymentMonths)) / (Math.pow(1 + monthlyInterest, numberOfPaymentMonths) - 1)
 })
 </script>
