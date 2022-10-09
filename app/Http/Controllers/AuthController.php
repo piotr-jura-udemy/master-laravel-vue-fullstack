e18a290cb9898069a17619d592c856d7c54dc359
Piotr Jura <jura.piotrek@gmail.com>
2022-10-09 19:03:37 +0200

    007-001-auth-controller-and-routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function create()
+    {
+    }
+
+    public function store()
+    {
+    }
+
+    public function destroy()
+    {
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
@@ -19,3 +20,10 @@ Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
 
 Route::resource('listing', ListingController::class);
+
+Route::get('login', [AuthController::class, 'create'])
+  ->name('login');
+Route::post('login', [AuthController::class, 'store'])
+  ->name('login.store');
+Route::delete('logout', [AuthController::class, 'destroy'])
+  ->name('logout');
