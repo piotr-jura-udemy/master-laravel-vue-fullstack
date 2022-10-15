254f4355f1a9ae53314e5ccd03eb4d5359e96180
Piotr Jura <jura.piotrek@gmail.com>
2022-10-15 16:42:09 +0200

    007-012-how-hashing-works

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class IndexController extends Controller
 {
@@ -22,8 +23,13 @@ class IndexController extends Controller
         //     'beds' => 2, 'baths' => 2, 'area' => 100, 'city' => 'North', 'street' => 'Tinker st', 'street_nr' => 20, 'code' => 'TS', 'price' => 200_000
         // ])
         // dd(Auth::user());
-
         // dd(Auth::check());
+
+        dd(
+            Hash::make('password'),
+            '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            Hash::check('password', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi')
+        );
 
         return inertia(
             'Index/Index',
