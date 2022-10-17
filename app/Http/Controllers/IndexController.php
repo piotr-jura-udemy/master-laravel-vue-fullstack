c3ea43d14091a999d8780272a0dc7815c750457e
Piotr Jura <jura.piotrek@gmail.com>
2022-10-17 18:31:15 +0200

    008-003-one-to-many-storing-practice

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -22,6 +22,9 @@ class IndexController extends Controller
         // Listing::create([
         //     'beds' => 2, 'baths' => 2, 'area' => 100, 'city' => 'North', 'street' => 'Tinker st', 'street_nr' => 20, 'code' => 'TS', 'price' => 200_000
         // ])
+        // Listing::make([
+        //     'beds' => 2, 'baths' => 2, 'area' => 100, 'city' => 'North', 'street' => 'Tinker st', 'street_nr' => 20, 'code' => 'TS', 'price' => 200_000
+        // ])
         // dd(Auth::user());
         // dd(Auth::check());
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test2@example.com',
+        ]);
         \App\Models\Listing::factory(20)->create([
             'by_user_id' => 1
         ]);
