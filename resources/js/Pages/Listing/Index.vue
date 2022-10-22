000b5d11b653ce84f51c0f554476cc5f3a8f5dd5
Piotr Jura <jura.piotrek@gmail.com>
2022-10-22 18:53:26 +0200

    009-002-filtering-form-markup

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,4 +1,5 @@
 <template>
+  <Filters />
   <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
     <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />
   </div>
@@ -11,6 +12,7 @@
 <script setup>
 import Listing from '@/Pages/Listing/Index/Components/Listing.vue'
 import Pagination from '@/Components/UI/Pagination.vue'
+import Filters from '@/Pages/Listing/Index/Components/Filters.vue'
 defineProps({
   listings: Object,
 })
---
+++ resources/js/Pages/Listing/Index/Components/Filters.vue
@@ -0,0 +1,43 @@
+<template>
+  <form>
+    <div class="mb-8 mt-4 flex flex-wrap gap-2">
+      <div class="flex flex-nowrap items-center">
+        <input
+          type="text" placeholder="Price from"
+          class="input-filter-l w-28"
+        />
+        <input
+          type="text" placeholder="Price to" 
+          class="input-filter-r w-28"
+        />
+      </div>
+
+      <div class="flex flex-nowrap items-center">
+        <select class="input-filter-l w-28">
+          <option :value="null">Beds</option>
+          <option v-for="n in 5" :key="n" :value="n">{{ n }}</option>
+          <option>6+</option>
+        </select>
+        <select class="input-filter-r w-28">
+          <option :value="null">Baths</option>
+          <option v-for="n in 5" :key="n" :value="n">{{ n }}</option>
+          <option>6+</option>
+        </select>
+      </div>
+
+      <div class="flex flex-nowrap items-center">
+        <input
+          type="text" placeholder="Area from"
+          class="input-filter-l w-28"
+        />
+        <input
+          type="text" placeholder="Area to"
+          class="input-filter-r w-28"
+        />
+      </div>
+
+      <button type="submit" class="btn-normal">Filter</button>
+      <button type="reset">Clear</button>
+    </div>
+  </form>
+</template>
