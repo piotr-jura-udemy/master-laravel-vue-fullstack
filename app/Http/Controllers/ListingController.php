ef93c9a7b5b64a57175e1189cff45ddfcbfa04c6
Piotr Jura <jura.piotrek@gmail.com>
2022-10-24 19:27:39 +0200

    009-004-filtering-data

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -20,14 +20,40 @@ class ListingController extends Controller
      */
     public function index(Request $request)
     {
+        $filters = $request->only([
+            'priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo'
+        ]);
+        $query = Listing::orderByDesc('created_at');
+
+        if ($filters['priceFrom'] ?? false) {
+            $query->where('price', '>=', $filters['priceFrom']);
+        }
+
+        if ($filters['priceTo'] ?? false) {
+            $query->where('price', '<=', $filters['priceTo']);
+        }
+
+        if ($filters['beds'] ?? false) {
+            $query->where('beds', $filters['beds']);
+        }
+
+        if ($filters['baths'] ?? false) {
+            $query->where('baths', $filters['baths']);
+        }
+
+        if ($filters['areaFrom'] ?? false) {
+            $query->where('area', '>=', $filters['areaFrom']);
+        }
+
+        if ($filters['areaTo'] ?? false) {
+            $query->where('area', '<=', $filters['areaTo']);
+        }
+
         return inertia(
             'Listing/Index',
             [
-                'filters' => $request->only([
-                    'priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo'
-                ]),
-                'listings' => Listing::orderByDesc('created_at')
-                    ->paginate(10)
+                'filters' => $filters,
+                'listings' => $query->paginate(10)
                     ->withQueryString()
             ]
         );
