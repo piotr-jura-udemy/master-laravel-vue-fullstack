be5fabe89b78db21b6e992694dcc8bc46f44224e
Piotr Jura <jura.piotrek@gmail.com>
2022-10-29 17:41:43 +0200

    010-004-soft-deletes

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Listing extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'beds', 'baths', 'area', 'city', 'code', 'street', 'street_nr', 'price'
