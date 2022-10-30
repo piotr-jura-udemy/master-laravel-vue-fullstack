9340932f99d9567e767f3474d5567587b23b815f
Piotr Jura <jura.piotrek@gmail.com>
2022-10-30 13:56:43 +0100

    010-006-filtering-booleans-front-end-markup

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -4,19 +4,11 @@
       <div class="flex flex-nowrap items-center gap-2">
         <input
           id="deleted"
-          v-model="filterForm.deleted"
-          type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+          type="checkbox" 
+          class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
         />
         <label for="deleted">Deleted</label>
       </div>
     </div>
   </form>
 </template>
-
-<script setup>
-import { useForm } from '@inertiajs/inertia-vue3'
-
-const filterForm = useForm({
-  deleted: false,
-})
-</script>
