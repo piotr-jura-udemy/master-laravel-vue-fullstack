ae440d8af73eb63dcd7596f17ee85aa1de50d0a6
Piotr Jura <jura.piotrek@gmail.com>
2022-10-30 15:07:11 +0100

    010-008-debounce

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -17,16 +17,17 @@
 <script setup>
 import { reactive, watch } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
+import {debounce} from 'lodash'
 
 const filterForm = reactive({
   deleted: false,
 })
 // reactive / ref / computed
 watch(
-  filterForm, () => Inertia.get(
+  filterForm, debounce(() => Inertia.get(
     route('realtor.listing.index'),
     filterForm,
-    {preserveState: true, preserveScroll: true},
-  ),
+    { preserveState: true, preserveScroll: true },
+  ), 1000),
 )
 </script>
