3b1f9fc34c033e2647273f408eb9b281f7a1ef5a
Piotr Jura <jura.piotrek@gmail.com>
2022-11-10 17:37:18 +0100

    012-007-offer-list

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -1,3 +1,44 @@
 <template>
-  Hello!
+  <div class="mb-4">
+    <Link 
+      :href="route('realtor.listing.index')"
+    >
+      ← Go back to Listings
+    </Link>
+  </div>
+
+  <section class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+    <Box v-if="!hasOffers" class="flex md:col-span-7 items-center">
+      <div class="w-full text-center font-medium text-gray-500">
+        No offers
+      </div>
+    </Box>
+
+    <div v-else class="md:col-span-7 items-center">
+      This is displayed when there are offers!
+    </div>
+
+    <Box class="md:col-span-5">
+      <template #header>Basic Info</template>
+      <Price :price="listing.price" class="text-2xl font-bold" />
+
+      <ListingSpace :listing="listing" class="text-lg" />
+      <ListingAddress :listing="listing" class="text-gray-500" />
+    </Box>
+  </section>
 </template>
+
+<script setup>
+import ListingAddress from '@/Components/ListingAddress.vue'
+import ListingSpace from '@/Components/ListingSpace.vue'
+import Price from '@/Components/Price.vue'
+import Box from '@/Components/UI/Box.vue'
+import { Link } from '@inertiajs/inertia-vue3'
+import { computed } from 'vue'
+
+const props = defineProps({ listing: Object })
+
+const hasOffers = computed(
+  () => props.listing.offers.length,
+)
+</script>
