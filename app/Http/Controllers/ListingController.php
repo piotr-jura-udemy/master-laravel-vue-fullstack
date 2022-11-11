19e2c87d92d1fc9dd536b4261e538261e30a5970
Piotr Jura <jura.piotrek@gmail.com>
2022-11-11 16:29:07 +0100

    012-011-querying-relation-existance

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -30,6 +30,7 @@ class ListingController extends Controller
                 'filters' => $filters,
                 'listings' => Listing::mostRecent()
                     ->filter($filters)
+                    ->withoutSold()
                     ->paginate(10)
                     ->withQueryString()
             ]
--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -43,6 +43,16 @@ class Listing extends Model
         return $query->orderByDesc('created_at');
     }
 
+    public function scopeWithoutSold(Builder $query): Builder
+    {
+        return $query->doesntHave('offers')
+            ->orWhereHas(
+                'offers',
+                fn (Builder $query) => $query->whereNull('accepted_at')
+                    ->whereNull('rejected_at')
+            );
+    }
+
     public function scopeFilter(Builder $query, array $filters): Builder
     {
         return $query->when(
