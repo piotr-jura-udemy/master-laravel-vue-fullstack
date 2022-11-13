1e9fc91074c5222d8a27177eb3f3e850460ab85a
Piotr Jura <jura.piotrek@gmail.com>
2022-11-13 19:21:51 +0100

    013-002-sending-database-notifcation

--- app/Http/Controllers/ListingOfferController.php
+++ app/Http/Controllers/ListingOfferController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Offer;
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use App\Notifications\OfferMade;
 
 class ListingOfferController extends Controller
 {
@@ -12,12 +13,15 @@ class ListingOfferController extends Controller
     {
         $this->authorize('view', $listing);
 
-        $listing->offers()->save(
+        $offer = $listing->offers()->save(
             Offer::make(
                 $request->validate([
                     'amount' => 'required|integer|min:1|max:20000000'
                 ])
             )->bidder()->associate($request->user())
+        );
+        $listing->owner->notify(
+            new OfferMade($offer)
         );
 
         return redirect()->back()->with(
