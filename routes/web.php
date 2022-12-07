f16b3201c4830419fadc9900d18218581ab56e99
Piotr Jura <jura.piotrek@gmail.com>
2022-12-07 19:55:37 +0100

    014-006-resend-verification-link

--- routes/web.php
+++ routes/web.php
@@ -64,6 +64,12 @@ Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $requ
     ->with('success', 'Email was verified!');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
+Route::post('/email/verification-notification', function (Request $request) {
+  $request->user()->sendEmailVerificationNotification();
+
+  return back()->with('success', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
 Route::resource('user-account', UserAccountController::class)
   ->only(['create', 'store']);
 
